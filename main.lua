ae02dc6cd6e2543193d6e465d640bad3c29a418b
Justin Collins <justin@presidentbeef.com>
2019-02-14 22:54:45 -0800

    Move love callbacks to end of file

--- main.lua
+++ main.lua
@@ -10,113 +10,6 @@ local function reset_enemies(width)
   return enemies
 end
 
-function love.load()
-  love.window.setMode(244 * 2, 288 * 2)
-  love.window.setTitle("Not Galaga")
-
-  local width, height, _ = love.window.getMode()
-  ship_max_w = width - 70
-  w_max_w = width
-  w_max_h = height
-  
-  ship = make_object(love.graphics.newImage("images/ship.png"), width / 2, height - 100)
-
-  ship.main_image = ship.image 
-  ship.left_image = love.graphics.newImage("images/ship_left.png")
-  ship.right_image = love.graphics.newImage("images/ship_right.png")
-
-  enemies = reset_enemies(width)
-  background = make_object(love.graphics.newImage("images/background1.png"), 0, -288 * 2)
-  background2 = make_object(love.graphics.newImage("images/background2.png"), 0, -288 * 2)
-
-  missile = {}
-  local missile_image = love.graphics.newImage("images/missile.png")
-  missile[0] = make_object(missile_image, -1, -1)
-  missile[1] = make_object(missile_image, -1, -1)
-
-  local img = love.graphics.newImage("images/red.png")
-
-  local psystem = love.graphics.newParticleSystem(img, 30)
-  psystem:setParticleLifetime(1, 2)
-  psystem:setEmissionRate(6)
-  psystem:setSizeVariation(0.1)
-  psystem:setLinearAcceleration(0, 10, 0, 10)
-  psystem:setColors(255, 255, 255, 255, 255, 255, 255, 0) -- Fade to transparency.
-
-  ship.engine = make_object(psystem, ship.x + 37, ship.y + 71) 
-  bg_flicker = 0
-  explosions = {}
-end
-
-function love.draw()
-  love.graphics.setColor(255, 255, 255, (223 + math.sin(bg_flicker) * 32))
-  draw_object(background)
-  love.graphics.reset()
-  draw_object(background2)
-  draw_object(ship.engine)
-  love.graphics.draw(ship.image, ship.x, ship.y, 0, 0.25, 0.25)
-  for _, e in ipairs(enemies) do
-    draw_object(e)
-  end
-  draw_missile(missile[0])
-  draw_missile(missile[1])
-  for _, e in ipairs(explosions) do
-    draw_object(e)
-  end
-end
-
-function love.update(dt)
-  ship_move(dt)
-  engine_move(dt)
-  for _, e in ipairs(enemies) do
-    enemy_move(e, dt)
-  end
-  missile_move(missile[0], dt)
-  missile_move(missile[1], dt)
-
-  local deleted = {}
-
-  for i, enemy in ipairs(enemies) do
-    if enemy:in_box(missile[0]) then
-      explode(missile[0].x, missile[0].y)
-      missile[0].y = -1000
-      enemy.y = -1000
-      table.insert(deleted, i)
-    elseif enemy:in_box(missile[1]) then
-      explode(missile[1].x, missile[1].y)
-      missile[1].y = -1000
-      table.insert(deleted, i)
-    end
-  end
-
-  background_move(dt)
-  bg_flicker = bg_flicker + dt
-
-  for _, e in ipairs(explosions) do
-    e.image:update(dt)
-  end
-
-  for _, d in ipairs(deleted) do
-    table.remove(enemies, d)
-  end
-
-  if #enemies == 0 then
-    enemies = reset_enemies(w_max_w)
-  end
-end
-
-function love.keypressed(key)
-  if key == " " or key == "a" or key == "s" then
-    if missile[0].y < 0 then
-      start_missile(missile[0], 0)
-    elseif missile[1].y < 0 then
-      start_missile(missile[1], 8)
-    end
-  elseif key == "escape" then
-    love.event.push('quit')
-  end
-end
-
 function start_missile(m, e)
   m.y = ship.y - 10
   m.x = ship.x + 29 + e
@@ -188,10 +81,10 @@ function draw_object(obj)
 end
 
 function in_box(self, obj)
-  return obj.x > self.x and
-  obj.x < self.x + self.w and
-  obj.y > self.y and
-  obj.y < self.y + self.h
+  return obj.y > self.y and
+  obj.y < self.y + self.h and
+  obj.x > self.x and
+  obj.x < self.x + self.w
 end
 
 function make_object(image, x, y)
@@ -216,7 +109,7 @@ function make_enemy(image_src, x, y, movement)
   enemy.max_w = w_max_w - enemy.image:getWidth()
 
   if movement then
-    enemy.movement = movement 
+    enemy.movement = movement
   else
     enemy.movement = right_left_down
   end
@@ -290,5 +183,112 @@ function opposite(enemy, dt)
     enemy.x = enemy.max_w
   elseif enemy.x < 0 then
     enemy.x = 0
+  end
+end
+
+function love.load()
+  love.window.setMode(244 * 2, 288 * 2)
+  love.window.setTitle("Not Galaga")
+
+  local width, height, _ = love.window.getMode()
+  ship_max_w = width - 70
+  w_max_w = width
+  w_max_h = height
+
+  ship = make_object(love.graphics.newImage("images/ship.png"), width / 2, height - 100)
+
+  ship.main_image = ship.image
+  ship.left_image = love.graphics.newImage("images/ship_left.png")
+  ship.right_image = love.graphics.newImage("images/ship_right.png")
+
+  enemies = reset_enemies(width)
+  background = make_object(love.graphics.newImage("images/background1.png"), 0, -288 * 2)
+  background2 = make_object(love.graphics.newImage("images/background2.png"), 0, -288 * 2)
+
+  missile = {}
+  local missile_image = love.graphics.newImage("images/missile.png")
+  missile[0] = make_object(missile_image, -1, -1)
+  missile[1] = make_object(missile_image, -1, -1)
+
+  local img = love.graphics.newImage("images/red.png")
+
+  local psystem = love.graphics.newParticleSystem(img, 30)
+  psystem:setParticleLifetime(1, 2)
+  psystem:setEmissionRate(6)
+  psystem:setSizeVariation(0.1)
+  psystem:setLinearAcceleration(0, 10, 0, 10)
+  psystem:setColors(255, 255, 255, 255, 255, 255, 255, 0) -- Fade to transparency.
+
+  ship.engine = make_object(psystem, ship.x + 37, ship.y + 71)
+  bg_flicker = 0
+  explosions = {}
+end
+
+function love.draw()
+  love.graphics.setColor(255, 255, 255, (223 + math.sin(bg_flicker) * 32))
+  draw_object(background)
+  love.graphics.reset()
+  draw_object(background2)
+  draw_object(ship.engine)
+  love.graphics.draw(ship.image, ship.x, ship.y, 0, 0.25, 0.25)
+  for _, e in ipairs(enemies) do
+    draw_object(e)
+  end
+  draw_missile(missile[0])
+  draw_missile(missile[1])
+  for _, e in ipairs(explosions) do
+    draw_object(e)
+  end
+end
+
+function love.update(dt)
+  ship_move(dt)
+  engine_move(dt)
+  for _, e in ipairs(enemies) do
+    enemy_move(e, dt)
+  end
+  missile_move(missile[0], dt)
+  missile_move(missile[1], dt)
+
+  local deleted = {}
+
+  for i, enemy in ipairs(enemies) do
+    if enemy:in_box(missile[0]) then
+      explode(missile[0].x, missile[0].y)
+      missile[0].y = -1000
+      enemy.y = -1000
+      table.insert(deleted, i)
+    elseif enemy:in_box(missile[1]) then
+      explode(missile[1].x, missile[1].y)
+      missile[1].y = -1000
+      table.insert(deleted, i)
+    end
+  end
+
+  background_move(dt)
+  bg_flicker = bg_flicker + dt
+
+  for _, e in ipairs(explosions) do
+    e.image:update(dt)
+  end
+
+  for _, d in ipairs(deleted) do
+    table.remove(enemies, d)
+  end
+
+  if #enemies == 0 then
+    enemies = reset_enemies(w_max_w)
+  end
+end
+
+function love.keypressed(key)
+  if key == " " or key == "a" or key == "s" then
+    if missile[0].y < 0 then
+      start_missile(missile[0], 0)
+    elseif missile[1].y < 0 then
+      start_missile(missile[1], 8)
+    end
+  elseif key == "escape" then
+    love.event.push('quit')
   end
 end
